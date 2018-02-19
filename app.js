df5b011ecefe718f053272b326ac51f870062d3d
Justin Lee <justinlee0022@gmail.com>
2018-02-19 08:08:02 -0800

    Completed implementation of section generator

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const c = require('./constants')
 const fs = require('fs')
 const util = require('util')
 const path = require('path')
-const assert = require('assert')
 
 const fswrite = util.promisify(fs.writeFile)
 const fsappend = util.promisify(fs.appendFile)
@@ -22,6 +21,7 @@ const getCoursesInDept = async (dept, year, term) => {
   const xml = await response.text()
   const json = xljs.xml2js(xml)
   const courseObjs = json.courses.course
+    .map(({ _key, _title }) => ({ course: _key, description: _title }))
   return courseObjs
 }
 
@@ -32,19 +32,24 @@ const getSectionsInCourse = async (dept, course) => {
   const sections = Array.isArray(json.sections.section) ? json.sections.section : [json.sections.section]
   const sectionsWithWaitListFiltered = sections.filter(section => section._activity !== 'Waiting List')
   const requiredFields = sectionsWithWaitListFiltered
-    .map(({ instructors = '', _activity, _credits }) =>
-      ({ instructor: instructors.instructor ? instructors.instructor._name : '', activty: _activity, credits: _credits }))
+    .map(({ instructors = '', _activity, _credits, _key }) =>
+      ({ instructor: instructors.instructor ? instructors.instructor._name : '', activity: _activity, credits: _credits, section: _key }))
   return requiredFields
 }
 
-const writeHeader = async header => fswrite(filepath, header)
-
-writeHeader(c.csvHeaders)
+const writeHeader = header => fswrite(filepath, header + '\r\n')
+const append = row => fsappend(filepath, row + '\r\n')
+const stringify = JSON.stringify
 
 c.departments.forEach(async dept => {
+  await writeHeader(c.csvHeaders)
   const courseObjs = await getCoursesInDept(dept, year, term)
-  courseObjs.forEach(async ({ _key, _title }) => {
-    const sections = await getSectionsInCourse(dept, _key)
-    console.log(sections)
+  courseObjs.forEach(async ({ course, description }) => {
+    const sections = await getSectionsInCourse(dept, course)
+    sections.forEach(async ({ instructor, activity, credits, section }) => {
+      const stringified = [ year, term, dept, course, section, instructor, credits, activity ]
+        .map(x => stringify(x))
+      await append(stringified)
+    })
   })
 })
